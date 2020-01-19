7dea8737a3af2e3611c42db5dd520976a43adcb7
Vincent Michel <vxgmichel@gmail.com>
2020-01-19 17:04:16 +0100

    Run winfstest operations in a dedicated subprocess

--- src/winfspy/tests/winfstest/test_winfs.py
+++ src/winfspy/tests/winfstest/test_winfs.py
@@ -2,14 +2,18 @@
 
 import os
 import uuid
+import types
 import pathlib
 from functools import partial
+from concurrent.futures import ProcessPoolExecutor
 
 import pytest
 import win32api
 import win32con
 import win32file
 import winerror
+import winnt
+import pywintypes
 
 
 # Load symbols and test modules
@@ -22,37 +26,102 @@ def get_test_modules(base="cases"):
             yield path
 
 
-SYMBOLS = {**win32api.__dict__, **win32con.__dict__, **win32file.__dict__}
 TEST_MODULES = list(get_test_modules())
+SYMBOL_DICT = {**win32api.__dict__, **win32con.__dict__, **win32file.__dict__, **winnt.__dict__}
+SYMBOLS = types.SimpleNamespace(**SYMBOL_DICT)
 
 
 # Operations
 
 
 def create_file(
-    path, desired_access, share_mode, sddl, creation_disposition, flags_and_attributes, zero=0
+    path,
+    desired_access,
+    share_mode,
+    sddl,
+    creation_disposition,
+    flags_and_attributes,
+    zero=0,
+    raise_last_error=False,
 ):
+    # Security descriptors are not supported in the tests at the moment
     assert zero == 0
     assert sddl == 0
-    handle = win32file.CreateFile(
+
+    # Windows API call
+    handle = win32file.CreateFileW(
         path, desired_access, share_mode, None, creation_disposition, flags_and_attributes, 0
     )
+
+    # Close the handle
+    # This is necessary since we use a single process
+    # for running all the commands from a single test case
     assert handle != win32file.INVALID_HANDLE_VALUE
-    return handle
+    win32file.CloseHandle(handle)
+
+    # Used when the command is prepended with `-e`
+    if raise_last_error:
+        raise pywintypes.error(win32api.GetLastError(), "CreateFileW", None)
 
 
 def delete_file(path):
+    # Windows API call
     win32file.DeleteFile(path)
 
 
 def get_file_information(path):
-    raise NotImplementedError
+    # First windows API call
+    handle = win32file.CreateFileW(
+        path,
+        SYMBOLS.FILE_READ_ATTRIBUTES,
+        SYMBOLS.FILE_SHARE_READ | SYMBOLS.FILE_SHARE_WRITE | SYMBOLS.FILE_SHARE_DELETE,
+        None,
+        SYMBOLS.OPEN_EXISTING,
+        SYMBOLS.FILE_FLAG_OPEN_REPARSE_POINT | SYMBOLS.FILE_FLAG_BACKUP_SEMANTICS,
+        0,
+    )
+
+    # Second windows API call
+    assert handle != win32file.INVALID_HANDLE_VALUE
+    file_info = win32file.GetFileInformationByHandle(handle)
+
+    # Close the handle
+    # This is necessary since we use a single process
+    # for running all the commands from a single test case
+    win32file.CloseHandle(handle)
+
+    # Craft the result dictionary
+    keys = (
+        "FileAttributes",
+        "CreationTime",
+        "LastAccessTime",
+        "LastWriteTime",
+        "VolumeSerialNumber",
+        "FileSizeHigh",
+        "FileSizeLow",
+        "NumberOfLinks",
+        "FileIndexHigh",
+        "FileIndexLow",
+    )
+    result = dict(zip(keys, file_info))
+
+    # Ignore time attributes for the moment
+    result["CreationTime"] = None
+    result["LastAccessTime"] = None
+    result["LastWriteTime"] = None
+    return result
+
+
+def set_file_attributes(path, file_attributes):
+    # Windows API call
+    win32file.SetFileAttributesW(path, file_attributes)
 
 
 OPERATIONS = {
     "CreateFile": create_file,
     "DeleteFile": delete_file,
     "GetFileInformation": get_file_information,
+    "SetFileAttributes": set_file_attributes,
 }
 
 
@@ -63,42 +132,36 @@ def unique_name():
     return str(uuid.uuid4()) + ".txt"
 
 
-def expect(base_path, cmd, expected):
+def expect(base_path, runner, cmd, expected):
     print(f"Running: {cmd}")
     print(f"-> expecting: {expected}")
 
     args = cmd.split()
-    get_last_error = False
+    kwargs = {}
     if args[0] == "-e":
         args = args[1:]
-        get_last_error = True
+        kwargs["raise_last_error"] = True
 
     def parse(x):
-        return eval(x, SYMBOLS)
+        return eval(x, SYMBOL_DICT)
 
     operation = OPERATIONS[args[0]]
     path = os.path.join(base_path, args[1])
     args = list(map(parse, args[2:]))
 
-    if not expected and not get_last_error:
-        operation(path, *args)
-        print("-> OK!")
+    if not expected or callable(expected):
+        result = runner(operation, path, *args, **kwargs)
+        if callable(expected):
+            assert expected((result,))
+        print(f"-> OK: {result}")
         return
 
-    try:
-        operation(path, *args)
-    except Exception as exc:
-        if type(exc).__name__ != "error":
-            raise
-        errno, _, message = exc.args
-    else:
-        if get_last_error:
-            errno = win32api.GetLastError()
-            message = None
-        else:
-            errno = message = None
+    with pytest.raises(pywintypes.error) as context:
+        runner(operation, path, *args, **kwargs)
+
+    errno, _, message = context.value.args
     assert errno == getattr(winerror, expected), f"Expected {expected}, got {message}"
-    print("-> OK!")
+    print(f"-> OK: expected errno {errno}")
 
 
 # Tests
@@ -109,10 +172,16 @@ def expect(base_path, cmd, expected):
 )
 def test_winfs(test_module_path, file_system_path):
     module_number = int(test_module_path.name[:2])
-    if module_number > 0:
+    if module_number > 1:
         pytest.xfail()
-    do_expect = partial(expect, file_system_path)
-    globs = {"uniqname": unique_name, "expect": do_expect, "testdone": lambda: None}
-    test_module = open(test_module_path).read()
-    test_module = test_module.replace("from winfstest import *", "")
-    exec(test_module, globs)
+
+    with ProcessPoolExecutor(max_workers=1) as executor:
+
+        def runner(fn, *args, **kwargs):
+            return executor.submit(fn, *args, **kwargs).result()
+
+        do_expect = partial(expect, file_system_path, runner)
+        globs = {"uniqname": unique_name, "expect": do_expect, "testdone": lambda: None}
+        test_module = open(test_module_path).read()
+        test_module = test_module.replace("from winfstest import *", "")
+        exec(test_module, globs)
