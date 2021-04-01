a786d8e28f06bc21a1accd4dd8ce16ac5b4364c0
Vincent Michel <vxgmichel@gmail.com>
2021-04-01 16:58:45 +0200

    Update python classifiers

--- setup.py
+++ setup.py
@@ -35,6 +35,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: BSD License",
     ],
     package_dir={"": "src"},
